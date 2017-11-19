d7976c42bb92c8e2f14f46a18e5324e67d908907
SamarV-121 <samarvispute121@gmail.com>
2017-11-19 14:57:12 +0530

    nicklaus: update trees

--- lineage.mk
+++ lineage.mk
@@ -1,4 +1,4 @@
-## Specify phone tech before including full_phone
+# Specify phone tech before including full_phone
 
 # Release name
 PRODUCT_RELEASE_NAME := nicklaus
@@ -9,9 +9,22 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Inherit device configuration
 $(call inherit-product, device/motorola/nicklaus/nicklaus.mk)
 
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+DEVICE_RESOLUTION := 720x1280
+
+# Bootanimation
+TARGET_BOOTANIMATION_PRELOAD := true
+TARGET_BOOTANIMATION_TEXTURE_CACHE := true
+
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := nicklaus
 PRODUCT_NAME := lineage_nicklaus
-PRODUCT_BRAND := Motorola
+PRODUCT_BRAND := motorola
 PRODUCT_MODEL := Moto E4 Plus
 PRODUCT_MANUFACTURER := Motorola
+
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
+
+# Available languages
+PRODUCT_LOCALES := en_US en_GB ru_RU uk_UA tr_TR sk_SK vi_VN fr_FR
